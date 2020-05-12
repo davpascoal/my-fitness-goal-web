b619f7d57d22ee386eb4715600559fec263a3770
David Pascoal <pascodav@Davids-MacBook-Pro.local>
2020-05-12 18:11:15 +0100

    Initialized routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeModule } from './views/home/home.module';
+import { HomeComponent } from './views/home/home.component';
 
+enum AppRoute {
+  HOME = '/',
+}
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('./views/home/home-routing.module').then(m => m.HomeRoutingModule),
+  },
+  {
+    path: '**',
+    redirectTo: AppRoute.HOME,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
